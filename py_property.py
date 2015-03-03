6703a6683c6c849f47a64af348c5c22d80f76f78
qingzh <sjtuzhq@gmail.com>
2015-03-03 18:06:40 +0800

    update @property py

---
+++ py_property.py
@@ -0,0 +1,19 @@
+class Test(object):
+    PROFILE = None
+
+    @property
+    def profile(self):
+        print 'get profile of %s' % self
+        return self.PROFILE
+    @profile.setter
+    def profile(self, value):
+        print 'set profile of %s' % self
+        if isinstance(value, int):
+            self.PROFILE = 'int: %s'% value
+        else:
+            self.PROFILE = value
+
+    def try_profile(self, value):
+        print 'try profile of %s' % self
+        # Do NOT use self.profile(value)
+        a.__class__.__dict__['profile'].__set__(self, value)
