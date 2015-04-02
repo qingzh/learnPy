a5a0fbba9e7d6cb8009abbdc9d0bf4a15225505b
zhangqing <zhangqing@xuetangx.com>
2015-04-02 16:27:40 +0800

    class decorator

--- py_decorators.py
+++ py_decorators.py
@@ -15,7 +15,36 @@ from time import time
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-def calculation_time(is_debug):
+
+def show_dir(func):
+    for i in dir(func):
+        if not hasattr(func, i):
+            continue
+        attr = getattr(func, i)
+        print '{}.{}: '.format(func.__class__.__name__, i), attr
+    return func
+
+class caclculateTime(object):
+    def __init__(self, is_debug):
+        print 'Start {}'.format(self.__class__.__name__) 
+        #show_dir(self)
+        self.is_debug = is_debug
+
+    def __call__(self, func):
+        def func_wrapper(*args, **kwargs):
+            if self.is_debug is True:
+                begin = time()
+            ret = func(*args, **kwargs)
+            if isinstance(ret, list):
+                print 'List: {}'.format(str(ret))
+            else:
+                print 'Return value is not a list'
+            if self.is_debug is True:
+                logging.info('\n Function [{}] finished in {:.10f}s'.format(func.__name__, time() - begin))
+            return ret
+        return func_wrapper
+
+def calculate_time(is_debug):
     def run_func(func):
         def func_wrapper(*args, **kwargs):
             if is_debug:
@@ -32,18 +61,27 @@ def func(name):
     for i in xrange(10000):
         i ** 3
 
-def test():
-    func('test')
+@caclculateTime(True)
+def test0():
+    func('test0')
+    return [1,2,3]
 
-@calculation_time(True)
+@caclculateTime(True)
+def test1():
+    func('test1')
+    return {'ret': False}
+
+@calculate_time(True)
 def test2():
     func('test2')
 
-@calculation_time(False)
+@calculate_time(False)
 def test3():
     func('test3')
 
 if __name__ == "__main__":
-    test()
+    test0()
     test2()
+    print '----'
+    test1()
     test3()
