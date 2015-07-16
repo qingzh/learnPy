93512eefc8e2ab4aeeb1d9a8671ffcd8383af06c
Qing <sjtuzhq@gmail.com>
2015-07-16 14:02:25 +0800

    change module variable

--- py_requests.py
+++ py_requests.py
@@ -1,14 +1,70 @@
-#!/usr/bin/env python
+
 # -*- coding:utf-8 -*-
 
-import requests
-
-
-# 怎么拦截302请求
 '''
-client_id: 1
-client_name: zhxy
-client_secret: c5cba62324ccd1bbccc700ba4a659729 
-{"ret":true,"scope":[2],"expire_in":150,"token_type":6,"refresh_token":"51f0f393-a000-47c8-a417-70aeac46d654","access_token":"d3880be5-089b-4c54-a0c2-432af064b15e"}
-
+ways to change `json.dumps` in requests
 '''
+from json import JSONEncoder
+
+
+class MyEncoder(JSONEncoder):
+
+    def default(self, obj):
+        if hasattr(obj, 'dict'):
+            return obj.dict()
+        super(MyEncoder, self).default(obj)
+
+
+def json_dump_decorator(func):
+    def json_dump_wrapper(*args, **kwargs):
+        kwargs['cls'] = MyEncoder
+        print args
+        print kwargs
+        return func(*args, **kwargs)
+    return json_dump_wrapper
+
+
+def test(c):
+    print c
+    req = c(url='http://google.com', json=DATA())
+    preq = req.prepare()
+    print preq.body
+
+
+class DATA(object):
+
+    def dict(self):
+        return {'test': 'hello', 'name': __file__}
+
+
+def test_a():
+    from requests import models
+
+    class ARequest(models.Request):
+        pass
+
+    try:
+        test(ARequest)
+    except Exception as e:
+        print e
+
+
+"""
+change variable `models.json_dumps`
+and you should customize Request Object
+"""
+
+
+def test_c():
+    from requests import models
+    from json import dumps
+
+    models.json_dumps = json_dump_decorator(dumps)
+
+    class CRequest(models.Request):
+        pass
+
+    try:
+        test(CRequest)
+    except Exception as e:
+        print e
