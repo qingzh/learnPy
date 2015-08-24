8a1e8fa71cba88b39eb6b2c9da1d4fab3b0af296
qingzh <sjtuzhq@gmail.com>
2015-08-24 18:57:29 +0800

    remove WebTest

--- WebTest/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__title__ = 'WebTest'
-__version__ = '1.0.0'
-__author__ = 'Qing Zhang'
-
-from .models import *
--- WebTest/exceptions.py
+++
@@ -1,7 +0,0 @@
-#! -*- coding:utf8 -*-
-
-__all__ = ['NoPerformanceEntry']
-
-
-class NoPerformanceEntry(Exception):
-    pass
